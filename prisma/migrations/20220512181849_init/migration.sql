014571f2b3b873619efbb3db44f7a8b501b5bbe3
ChristianDeM <Christian.d.martino@gmail.com>
2022-05-12 13:19:19 -0500

    Creando migration

---
+++ prisma/migrations/20220512181849_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "manga" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "editorial" VARCHAR(255) NOT NULL,
+    "mission" INTEGER NOT NULL,
+    "tomosN" INTEGER NOT NULL,
+    "precio" INTEGER NOT NULL,
+    "estatus" BOOLEAN NOT NULL DEFAULT false,
+    "dateCreated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastUpdated" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "manga_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "manga_name_key" ON "manga"("name");
